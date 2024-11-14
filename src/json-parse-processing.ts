d745b7f5329e1ad115910c0e1d6732ef1f76eb1e
kevalthakarar <thakararkeval1010@gmail.com>
2024-11-14 17:58:00 +0530

    wip: json parsing

--- src/json-parse-processing.ts
+++ src/json-parse-processing.ts
@@ -5,8 +5,9 @@ export class JSONParseProcessing {
     private tState: TokenStates;
     private key: undefined;
     private value: undefined;
-    private string: undefined;
+    private string: string | undefined;
     private stringBuffer: Buffer;
+    private stringBufferOffset: number;
     private unicode: undefined;
     private highSurrogate: undefined;
     private mode: ParserMode;
@@ -25,8 +26,8 @@ export class JSONParseProcessing {
         this.tState = TokenStates.START;
         this.key = undefined;
         this.value = undefined;
-        this.string = undefined;
         this.stringBuffer = Buffer.alloc(STRING_BUFFER_SIZE);
+        this.stringBufferOffset = 0;
         this.unicode = undefined;
         this.highSurrogate = undefined;
         this.mode = ParserMode.OBJECT;
@@ -48,12 +49,74 @@ export class JSONParseProcessing {
 
         for(let i = 0; i < buffer.length; i++){
             if(this.tState == TokenStates.START){
+                this.offset++;
                 this.parseStartState(buffer[i]);
             }
         }
     }
 
     private parseStartState(num: number) {
-        
+        switch(num){
+            case ParserToken.LEFT_BRACE: {
+                break;
+            }
+
+            case ParserToken.RIGHT_BRACE: {
+                break;
+            }
+
+            case ParserToken.LEFT_BRACKET: {
+                break;
+            }
+
+            case ParserToken.RIGHT_BRACKET: {
+                break;
+            }
+
+            case ParserToken.COLON: {
+                break;
+            }
+
+            case ParserToken.COMMA: {
+                break;
+            }
+
+            case ParserToken.TRUE: {
+                this.tState = TokenStates.TRUE1;
+                break;
+            }
+
+            case ParserToken.FALSE: {
+                this.tState = TokenStates.FALSE1;
+                break;
+            }
+
+            case ParserToken.NULL: {
+                this.tState = TokenStates.NULL1;
+                break;
+            }
+
+            case ParserToken.STRING: {
+                this.string = "";
+                this.stringBufferOffset = 0;
+                this.tState = TokenStates.STRING1;  // Normal string initilization
+                break;
+            }
+
+            default: {
+                return this.unknownCharacterError(num , this.offset);
+            }
+        }
+    }
+
+
+
+
+
+
+
+    private unknownCharacterError(num: number, i: number) {
+        this.tState = TokenStates.STOP;
+        throw new Error(`Unexpected ${String.fromCharCode(num)} at position ${i}`);
     }
 }
